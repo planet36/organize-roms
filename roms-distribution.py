ded26f802720f4511f9f48691010dffd4ed5380d
planet36 <planet36@users.noreply.github.com>
2022-04-28 13:45:55 -0500

    Add script to determine ROMs distribution

---
+++ roms-distribution.py
@@ -0,0 +1,100 @@
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+'''
+Usage: python3 roms-distribution.py ROMS_DIR...
+
+For each ROMS_DIR, print the distribution of the first alpha-numeric character of the filenames.
+The results are used to determine an optimal subdirectory structure into which ROMs could be split.
+'''
+
+from shlex import quote
+from collections import OrderedDict
+from pathlib import Path
+import sys
+
+__author__ = 'Steven Ward'
+__version__ = '2022-04-28'
+
+def get_first_alnum(s: str):
+	'''Find the first alpha-numeric character in the string.'''
+	for c in s:
+		if c.isalnum():
+			return c
+	return None
+
+def roms_distribution(path: Path) -> OrderedDict:
+	'''Get the distribution of the first alpha-numeric character of the filenames in the path.'''
+
+	bios_pattern = '[BIOS]'
+	num_pattern = '[0-9]'
+
+	dist = OrderedDict()
+
+	dist[bios_pattern] = 0
+	dist[num_pattern] = 0
+	dist['A'] = 0
+	dist['B'] = 0
+	dist['C'] = 0
+	dist['D'] = 0
+	dist['E'] = 0
+	dist['F'] = 0
+	dist['G'] = 0
+	dist['H'] = 0
+	dist['I'] = 0
+	dist['J'] = 0
+	dist['K'] = 0
+	dist['L'] = 0
+	dist['M'] = 0
+	dist['N'] = 0
+	dist['O'] = 0
+	dist['P'] = 0
+	dist['Q'] = 0
+	dist['R'] = 0
+	dist['S'] = 0
+	dist['T'] = 0
+	dist['U'] = 0
+	dist['V'] = 0
+	dist['W'] = 0
+	dist['X'] = 0
+	dist['Y'] = 0
+	dist['Z'] = 0
+
+	for child in path.iterdir():
+		if child.is_file():
+			s = child.stem
+			key = None
+			if s.startswith(bios_pattern):
+				key = bios_pattern
+			else:
+				first_alnum = get_first_alnum(s.casefold())
+				if first_alnum is None:
+					print(f'# Warning: No alpha-numeric characters found in {quote(s)}', file=sys.stderr)
+					continue
+				if first_alnum.isalpha():
+					key = first_alnum.upper()
+				else: # isdecimal or isdigit or isnumeric
+					key = num_pattern
+			dist[key] += 1
+
+	return dist
+
+# pylint: disable=missing-function-docstring
+def main(argv = None):
+
+	if argv is None:
+		argv = sys.argv[1:]
+
+	for arg in argv:
+		print(f'# {arg}')
+
+		dist = roms_distribution(Path(arg))
+
+		key_max_len = len(max(dist.keys(), key=len))
+
+		for key, val in dist.items():
+			print(f"{key:<{key_max_len}} {val}")
+		print()
+
+if __name__ == '__main__':
+	sys.exit(main())
