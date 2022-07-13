80f6a5cd2aec4274a7aaf61cbe6c0e3b920f5d49
planet36 <planet36@users.noreply.github.com>
2022-07-13 10:22:34 -0500

    Add examples

--- roms-list/create-empty-roms-files.bash
+++ roms-list/create-empty-roms-files.bash
@@ -2,6 +2,15 @@
 # SPDX-License-Identifier: OSL-3.0
 
 # Run this script from the target directory.
+# Examples:
+# bash create-empty-roms-files.bash Nintendo-Game-Boy
+# bash create-empty-roms-files.bash Nintendo-Game-Boy-Color
+# bash create-empty-roms-files.bash Nintendo-Nintendo-Entertainment-System-unheadered
+# bash create-empty-roms-files.bash Nintendo-Super-Nintendo-Entertainment-System
+# bash create-empty-roms-files.bash Sega-32X
+# bash create-empty-roms-files.bash Sega-Game-Gear
+# bash create-empty-roms-files.bash Sega-Master-System-Mark-III
+# bash create-empty-roms-files.bash Sega-Mega-Drive-Genesis
 
 # shellcheck disable=SC2034
 
